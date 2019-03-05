9aa35390c0b77f23da9a693692d4d51493a69ada
Josh McCormick <josh.mccormick81@gmail.com>
2019-03-05 15:47:03 -0500

    updated

--- app.js
+++ app.js
@@ -4,26 +4,31 @@ $(function () {
         $('.portfolioArea').addClass('invisible');
         $('#homeTab').removeClass('text-white bg-dark');
         $('#portTab').removeClass('text-white bg-dark');
+        $('#quoteIntro').removeClass('invisible');
     }
     const getAbout = function () {
-        $('#homeTab').addClass('text-white bg-dark');
+        // $('#homeTab').addClass('text-white bg-dark');
         $('#portTab').removeClass('text-white bg-dark');
         $('.portfolioArea').addClass('invisible');
         $('#projectArea').addClass('invisible');
         $('.aboutMe').removeClass('invisible')
+        $('#quoteIntro').addClass('invisible');
+
     }
     const getPortfolio = function () {
         $('#homeTab').removeClass('text-white bg-dark');
         $('.aboutMe').addClass('invisible');
         $('.portfolioArea').removeClass('invisible');
         $('#projectArea').addClass('invisible');
-        $('#portTab').addClass('text-white bg-dark');
+        $('#quoteIntro').addClass('invisible');
+        // $('#portTab').addClass('text-white bg-dark');
     }
 
 
     // $('.jumbotron').on('click', hideAll);
     $('#homeTab').on('click', getAbout);
     $('#portTab').on('click', getPortfolio);
+    $('#bannerPic').on('click',hideAll);
     $('.contactbutton').hover(function () {
         $(this).toggleClass('bg-dark')
     });
@@ -33,19 +38,22 @@ $(function () {
     $('.office').mouseleave(function () {
         $('.officeTheme')[0].pause();
     });
+    $('.nav-item').on('click',function(){
+        $('.nav-item').removeClass('active');
+        $(this).addClass('active');
+    })
     $('.nav-link').hover(function () {
-        $(this).toggleClass('text-danger')
+        $(this).toggleClass('text-primary')
     })
     $('.list-group-item').hover(function () {
         $(this).toggleClass('bg-dark text-white')
     })
-    $('.list-group-item').on('click', function (e) {
-        e.preventDefault();
+    //toggle portfolio images
+    $('.portDisplay').on('click', function () {
         $('#projectArea').removeClass('invisible');
-        $('.list-group-item').removeClass('bg-danger')
-        $(this).addClass('bg-danger');
-        $('.media').addClass('invisible')
-        $(`#${$(this).data('val')}`).toggleClass('invisible');
+        $('.portDisplay').removeClass('bg-primary');
+        $('.media').addClass('invisible');
+        $(`#${$(this).attr('alt')}`).toggleClass('invisible');
     });
 })
 
